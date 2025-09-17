302112df68b001b66a02f8c391c7bcd216a3c8be
lwz <liangweizhu@siteshun.com>
2025-09-17 20:32:45 +0800

    fix: 修复ws参数类型标注错误问题

--- packages/mmjs-core/src/client/ws.ts
+++ packages/mmjs-core/src/client/ws.ts
@@ -7,11 +7,6 @@ export interface WebSocketClientStateImpl {
     maxReconnectAttempts: number;
     reconnectDelay: number;
     reconnectAttempts: number;
-}
-export interface WebSocketClientOptions {
-    heartbeatInterval?: number;
-    reconnectDelay?: number;
-    maxReconnectAttempts?: number;
     connect(): void;
     send(data: any): void;
     close(): void;
@@ -19,6 +14,11 @@ export interface WebSocketClientOptions {
     onMessage(this: WebSocket, ev: MessageEvent): any;
     onClose(this: WebSocket, ev: CloseEvent): any
     onError(this: WebSocket, error: Event): any;
+}
+export interface WebSocketClientOptions {
+    heartbeatInterval?: number;
+    reconnectDelay?: number;
+    maxReconnectAttempts?: number;
 }
 
 /**
